04e82e91244c2cf95b7ad7ceffb2dd3928e2eddb
Emil 'Skeen' Madsen <sovende@gmail.com>
2015-09-20 13:30:05 +0200

    Updated the gitignore and the converter file.

--- OctoSimple.sh
+++ OctoSimple.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+BUILD_DIR=build
+WORK_DIR=tmp
+DEBUG=true
+
+mkdir -p $BUILD_DIR
+mkdir -p $WORK_DIR
+
+# Setup the work dir environment
+cd $WORK_DIR
+
 # Get the source
-git clone https://github.com/Skeen/OctoPrint.git
+git clone https://github.com/Skeen/OctoPrint.git 
 
 # Remove the junk
 rm OctoPrint/*
+rm -rf OctoPrint/.*
 rm -rf OctoPrint/docs
 rm -rf OctoPrint/scripts
 rm -rf OctoPrint/tests
 rm -rf OctoPrint/translations
-rm -rf OctoPrint/src/octoprint_setup_tools
+rm -rf OctoPrint/src/octoprint_setuptools
 rm -rf OctoPrint/src/octoprint/filemanager
 rm -rf OctoPrint/src/octoprint/plugin
 rm -rf OctoPrint/src/octoprint/plugins
@@ -20,7 +31,38 @@ rm -rf OctoPrint/src/octoprint/translations
 rm -rf OctoPrint/src/octoprint/util
 rm OctoPrint/src/octoprint/*
 
+# Return to the root dir
+cd ..
+
 # Prepare the OctoSimple directory
-mkdir -p OctoSimple
-mv OctoPrint/src/octoprint/static/* OctoSimple
-mv OctoPrint/src/octoprint/templates/* OctoSimple
+mv $WORK_DIR/OctoPrint/src/octoprint/static/* $BUILD_DIR
+#mv OctoPrint/src/octoprint/templates/* $BUILD_DIR
+
+# Assert we got everything
+FILES_LEFT=$(find $BUILD_DIR/OctoPrint/ -type f)
+# TODO: ADD CHECK
+
+# Start converting
+SNIPPETS_DIR=$WORK_DIR/snippets
+mkdir -p $SNIPPETS_DIR
+
+TEMPLATES_DIR=$WORK_DIR/OctoPrint/src/octoprint/templates
+
+STYLESHEETS_JINJA=$TEMPLATES_DIR/stylesheets.jinja2
+./scripts/stylesheets.sh $BUILD_DIR > $SNIPPETS_DIR/stylesheets.snip
+rm $STYLESHEETS_JINJA
+
+# TODO: improve this
+INITSCRIPT_JINJA=$TEMPLATES_DIR/initscript.jinja2
+cp res/initscript.snip $SNIPPETS_DIR/initscript.snip
+rm $INITSCRIPT_JINJA
+
+JAVASCRIPTS_JINJA=$TEMPLATES_DIR/javascripts.jinja2
+./scripts/javascrips.sh $BUILD_DIR > $SNIPPETS_DIR/javascripts.snip
+rm $JAVASCRIPTS_JINJA
+
+./scripts/index-process.sh
+cp $WORK_DIR/index.html $BUILD_DIR
+
+# ... and apply the changes
+
